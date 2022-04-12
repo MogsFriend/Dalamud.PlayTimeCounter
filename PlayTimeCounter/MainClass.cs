60786eff713fc0a04facd549f1db1ce292b6d920
Arcturus <46330334+MogsFriend@users.noreply.github.com>
2022-04-12 16:25:52 +0900

    timer class change

--- PlayTimeCounter/MainClass.cs
+++ PlayTimeCounter/MainClass.cs
@@ -16,45 +16,48 @@ namespace PlayTimeCounter
         [PluginService] public static ClientState ClientState { get; private set; } = null!;
 
         public string Name => "PlayTimeCounter";
-        internal static Timer Timer;
+        internal static System.Timers.Timer Timer;
         internal static Stopwatch Elapse;
 
         public PlayTimeCounter()
         {
             Elapse = new Stopwatch();
             Elapse.Start();
-            Timer = new Timer(CallBack);
-            Timer.Change(0, 1000);
+            Timer = new System.Timers.Timer();
+            Timer.Interval = 1000;
+            Timer.Elapsed += Ticker;
+            Timer.Start();
             ClientState.Login += ClientState_Login;
         }
 
-        private void CallBack(object status)
+        private void Ticker(object sender, System.Timers.ElapsedEventArgs e)
         {
             long time = Elapse.ElapsedMilliseconds;
             if (time < 1000) return;
             string message = "";
             long elapsed = time / 3600000;
-            bool flag = elapsed > 1500;
-            if (time / 1000 % 3600 == 0)
+            bool flag_a = time / 1000 % 3600 == 0;
+            if (!flag_a)
             {
+                bool flag_b = elapsed > 1500;
                 switch (ClientState.ClientLanguage)
                 {
                     case Dalamud.ClientLanguage.Japanese:
                         // in logmessage.exd (409)
-                        message = $"ゲームを開始してから{(flag ? "1500時間以上" : elapsed + "時間")}が経過しました。\n過剰なゲームの利用は正常な日常生活に支障を与える恐れがあります。";
+                        message = $"ゲームを開始してから{(flag_b ? "1500時間以上" : elapsed + "時間")}が経過しました。\n過剰なゲームの利用は正常な日常生活に支障を与える恐れがあります。";
                         break;
                     case Dalamud.ClientLanguage.English:
                         // in logmessage.exd (409)
                         // message = $"You've played more than you should. It's time you got some vitamin D.";
-                        message = $"You've played more than you should. {(flag ? "More then 1500 hours" : elapsed + " hours")} have passed since you started the game.";
+                        message = $"You've played more than you should. {(flag_b ? "More then 1500 hours" : elapsed + " hours")} have passed since you started the game.";
                         break;
                     case Dalamud.ClientLanguage.German:
-                        // google translate
-                        message = $"{(flag ? "Mehr als 1500" : elapsed)} Stunden sind vergangen, seit ich das Spiel gestartet habe.\nÜbermäßiger Gebrauch von Spielen kann den normalen Alltag beeinträchtigen.";
+                        // google translated
+                        message = $"{(flag_b ? "Mehr als 1500" : elapsed)} Stunden sind vergangen, seit ich das Spiel gestartet habe.\nÜbermäßiger Gebrauch von Spielen kann den normalen Alltag beeinträchtigen.";
                         break;
                     case Dalamud.ClientLanguage.French:
-                        // google translate
-                        message = $"{(flag ? "Plus de 1500 " : elapsed)} heures se sont écoulées depuis que j'ai commencé le jeu.\nL'utilisation excessive de jeux peut interférer avec la vie quotidienne normale.";
+                        // google translated
+                        message = $"{(flag_b ? "Plus de 1500 " : elapsed)} heures se sont écoulées depuis que j'ai commencé le jeu.\nL'utilisation excessive de jeux peut interférer avec la vie quotidienne normale.";
                         break;
                 }
 
@@ -81,6 +84,7 @@ namespace PlayTimeCounter
         public void Dispose()
         {
             Elapse.Stop();
+            Timer.Stop();
             Timer.Dispose();
             ClientState.Login -= ClientState_Login;
             GC.SuppressFinalize(this);
